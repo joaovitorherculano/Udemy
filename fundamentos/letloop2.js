a186f4f8fe895e27642ad2213be0e23c20351434
João Vitor Herculano Gonçalves <joao29u@gmail.com>
2025-09-04 11:10:53 -0300

    novos ex

--- fundamentos/letloop2.js
+++ fundamentos/letloop2.js
@@ -1,10 +1,10 @@
 const funcs = []
 
-for (var i = 0; i < 10; i++) {
+for (let i = 0; i < 10; i++) {
     funcs.push(function() {
         console.log(i)
     })
 }   
 
-funcs[2]() // 10
-funcs[8]() // 10
+funcs[2]() // 2
+funcs[8]() // 8 
