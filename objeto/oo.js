c45aa0a0119051caaa1a71b651b47ba428ff46b9
João Vitor Herculano Gonçalves <joao29u@gmail.com>
2025-10-23 00:19:31 -0300

    Create oo.js

---
+++ objeto/oo.js
@@ -0,0 +1,22 @@
+// CODIGO NAO EXECUTAVEL!!!
+
+// PROCEDURAL
+processamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+    valor1: 1,
+    valor2: 2, 
+    valor3: 3,
+    processamento() {
+        // ...
+    }
+}
+
+objeto.processamento() // Foco passou a ser o objeto
+
+// Principios importantes:
+// 1. Abstração
+// 2. Encapsulamento
+// 3. Herança (prototype)
+// 4. Polimorfismo
